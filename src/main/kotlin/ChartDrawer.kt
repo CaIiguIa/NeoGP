0728f412011908ebee16b0202b33a1dca7e26ea4
CaIiguIa <sdjurecki@gmail.com>
2023-10-21 00:03:37 +0200

    charting single function

--- src/main/kotlin/ChartDrawer.kt
+++ src/main/kotlin/ChartDrawer.kt
@@ -1,6 +1,4 @@
-import com.aspose.cells.ChartType
-import com.aspose.cells.SaveFormat
-import com.aspose.cells.Workbook
+import com.aspose.cells.*
 import java.io.File
 import java.nio.file.Files
 import java.util.logging.Logger
@@ -12,30 +10,95 @@ class ChartDrawer {
 
         val log = Logger.getLogger(this::class.java.toString())
 
-        fun drawChart(inputFile: File, solution: String) {
+        fun drawChart(inputFile: File, problem: Problem, solution: String) {
+            val variableNumber = getVariableNumber(problem.f)
             val outCsvPath = inputFile.absolutePath.replace("input", "output").replace(".dat", ".csv")
             val outFilePath = inputFile.absolutePath.replace("input", "output").replace(".dat", ".xlsx")
             val content = Files.readString(inputFile.toPath(), Charsets.UTF_8)
-                .replace(Properties.getTinyGPProperties(1) + "\n", "")
-                .replace(Properties.getTinyGPProperties(2) + "\n", "")
+                .replace(Properties.getTinyGPProperties(variableNumber) + "\n", "")
+                .replace(".", ",")
+                .replace(" ", ";")
 
             val csvFile = File(outCsvPath)
             csvFile.parentFile.mkdirs()
             csvFile.createNewFile()
-            csvFile.writeText(content.replace(" ", ","))
-            chart(csvFile)
+            csvFile.writeText(content)
+
+            when (problem.f) {
+                is SingleFunction -> chartSingleFunction(csvFile, problem, solution)
+                is DoubleFunction -> chartDoubleFunction(csvFile, problem, solution)
+                else -> throw UnsupportedOperationException()
+            }
+
         }
 
+        private fun chartSingleFunction(csvFile: File, problem: Problem, solution: String) {
+            val solutionCol = getSolutionExcelColumn(problem.f)
+            val cellRange = "A1:$solutionCol${Properties.numberOfSteps + 1}"
 
-        private fun chart(csvFile: File) {
-            val workbook = Workbook(csvFile.absolutePath)
+            //open Excel file
+            val oTxtLoadOptions = TxtLoadOptions(LoadFormat.CSV)
+            oTxtLoadOptions.separator = ';'
+            oTxtLoadOptions.encoding = Encoding.getUTF8()
+            val workbook = Workbook(csvFile.absolutePath, oTxtLoadOptions)
             val worksheet = workbook.worksheets[0]
+            worksheet.cells[solutionCol + "1"].setSharedFormula(
+                getSolutionFormula(problem, solution),
+                Properties.numberOfSteps,
+                1
+            )
+
+            //chart
             val chartIndex = worksheet.charts.add(ChartType.COLUMN, 5, 5, 15, 15)
             val chart = worksheet.charts[chartIndex]
+            chart.setChartDataRange(cellRange, true)
 
-            chart.setChartDataRange("A1:C4", true)
             workbook.save(csvFile.absolutePath.replace(".csv", ".xlsx"), SaveFormat.XLSX)
         }
+
+
+        private fun chartDoubleFunction(csvFile: File, problem: Problem, solution: String) {
+            val solutionCol = getSolutionExcelColumn(problem.f)
+            val cellRange = "A1:$solutionCol${Properties.numberOfSteps + 1}"
+
+            //open Excel file
+            val oTxtLoadOptions = TxtLoadOptions(LoadFormat.CSV)
+            oTxtLoadOptions.separator = ';'
+            oTxtLoadOptions.encoding = Encoding.getUTF8()
+            val workbook = Workbook(csvFile.absolutePath, oTxtLoadOptions)
+            val worksheet = workbook.worksheets[0]
+
+            //change shared formula so it takes multiple columns as input
+            worksheet.cells[solutionCol + "1"].setSharedFormula(
+                getSolutionFormula(problem, solution),
+                Properties.numberOfSteps,
+                1
+            )
+
+            //chart
+            val chartIndex = worksheet.charts.add(ChartType.COLUMN, 5, 5, 15, 15)
+            val chart = worksheet.charts[chartIndex]
+            chart.setChartDataRange(cellRange, true)
+
+            workbook.save(csvFile.absolutePath.replace(".csv", ".xlsx"), SaveFormat.XLSX)
+        }
+
+        private fun getVariableNumber(function: Function) = when (function) {
+            is SingleFunction -> 1
+            is DoubleFunction -> 2
+            else -> throw UnsupportedOperationException()
+        }
+
+        private fun getSolutionExcelColumn(function: Function) = when (getVariableNumber(function)) {
+            1 -> "C"
+            2 -> "D"
+            else -> throw UnsupportedOperationException()
+        }
+
+        private fun getSolutionFormula(problem: Problem, solution: String) =
+            solution
+                .replace("X1", "A1")
+                .replace("X2", "B1")
 
     }
 }
--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -3,9 +3,9 @@ import java.nio.file.Paths
 
 fun main() {
     System.setProperty("java.util.logging.SimpleFormatter.format", "[%1\$tF %1\$tT] [%4$-7s] %5\$s %n")
-//    DataGenerator.generateInputs()
+    DataGenerator.generateInputs()
     File(Paths.get("").toAbsolutePath().toString() + "/src/main/resources/inputs").listFiles()?.forEach {
-        ChartDrawer.drawChart(it, "")
+        ChartDrawer.drawChart(it, Properties.data.first(), "=X1+1")
     }
 }
 
