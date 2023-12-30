07c3f5c82d646fc1df89ffa8fd920a6531eec112
kuvuś <kuba17ism@tuta.io>
2023-12-30 19:08:55 +0100

    Change fitness cases files format

--- src/main/kotlin/neoGP/NeoGP.kt
+++ src/main/kotlin/neoGP/NeoGP.kt
@@ -27,18 +27,17 @@ class NeoGP {
         fun loadParams(filePath: String): Data {
             // FILE FORMAT:
             // population_size=0, max_gen_count=0, max_depth=0, time_limit=0, fit_function=fitExact
-            // input_count output_count
-            // input1 input2 ... inputN output1 output2 ... outputM
+            // input1 input2 ... inputN ; output1 output2 ... outputM
             // ...
-            // input1 input2 ... inputN output1 output2 ... outputM
+            // input1 input2 ... inputN ; output1 output2 ... outputM
 
             val file = File(filePath)
             if (!file.exists())
                 throw IllegalStateException("Input file does not exist!")
             val lines = file.readLines()
 
-            if (lines.size < 3)
-                throw Exception("Error while parsing $filePath:  too few configuration lines, expected 3+ got ${lines.size}")
+            if (lines.size < 2)
+                throw Exception("Error while parsing $filePath:  too few configuration lines, expected 2+ got ${lines.size}")
 
             // Read NeoGP params
 
@@ -60,25 +59,19 @@ class NeoGP {
             NeoProperties.MAX_INT_VALUE = gpParams["max_int_value"]!!.toInt()
             NeoProperties.BEST_FITNESS_THRESHOLD = gpParams["best_fitness_threshold"]!!.toInt()
 
-            val ioParams = lines[1].split(" ")
-            if (ioParams.size != 2)
-                throw Exception("Error while parsing $filePath:  wrong number of IO counts, expected 2 got ${ioParams.size}")
-            val inputCount = ioParams[0].toInt()
-            val outputCount = ioParams[1].toInt()
-
             val paramsList: MutableList<Params> = mutableListOf()
 
             // Read count of inputs and outputs
-            for (line in lines.drop(2)) {
+            for (line in lines.drop(1)) {
                 if (line.isBlank())
                     break
 
-                val ios = line.split(" ")
-                if (ios.size != inputCount + outputCount)
-                    throw Exception("Error while parsing $filePath:  wrong number of inputs/outputs, expected ${inputCount + outputCount} got ${ios.size}")
+                val ios = line.split(";")
+                if (ios.size != 2)
+                    throw Exception("Error while parsing $filePath:  wrong number of inputs/outputs, expected 2 got ${ios.size}")
 
-                val inputs = ios.subList(0, inputCount)
-                val outputs = ios.subList(inputCount, inputCount + outputCount).map { it.trim() }
+                val inputs = ios[0].trim().split(" ").map { it.trim() }
+                val outputs = ios[1].trim().split(" ").map { it.trim() }
 
                 paramsList.add(Params(inputs, outputs))
             }
